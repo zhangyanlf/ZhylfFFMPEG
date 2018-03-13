168250c59a7ce63179e4d06bf67c79082ed42fc3
Zhylf <zhyl529@163.com>
2018-03-13 17:01:38 +0800

    创建FFMPEG工程

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ZhylfFFMPEG
+
+# 创建工程集成FFMPEG      （FFMPEG工程模板）
---
+++ ZhylfFFMPEG/ZhylfFFMPEG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZhylfFFMPEG
+//
+//  Created by 张彦林 on 2018/3/13.
+//  Copyright © 2018年 zhangyanlf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
